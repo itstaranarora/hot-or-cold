efec6aa762d0c252e64e7e0181d36b133aac4cf9
Taran Arora <42945114+itstaranarora@users.noreply.github.com>
2021-03-24 14:55:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import { Grid, Typography, Paper, Divider, Button } from '@material-ui/core';
-import Form from './Components/form';
-import { getInitialState, getFeedback } from './util';
-import Progress from './Components/progress';
-import './App.css';
-import Info from './Components/info'
-
+import React from "react";
+import {
+  Typography,
+  Paper,
+  Divider,
+  Button,
+  Container,
+} from "@material-ui/core";
+import Form from "./Components/form";
+import { getInitialState, getFeedback } from "./util";
+import Progress from "./Components/progress";
+import "./App.css";
+import Info from "./Components/info";
 
 class App extends React.Component {
   state = getInitialState();
@@ -15,44 +20,61 @@ class App extends React.Component {
   updateAppState = (guess) => {
     const { actual } = this.state;
     const absDiff = Math.abs(guess - actual);
-    const {feedbackMessage, feedbackColor } = getFeedback(absDiff);
+    const { feedbackMessage, feedbackColor } = getFeedback(absDiff);
 
-    this.setState(prevState => ({
+    this.setState((prevState) => ({
       guess,
-      allGuesses: [...prevState.allGuesses, {guess, feedbackColor }],
+      allGuesses: [...prevState.allGuesses, { guess, feedbackColor }],
       attempt: prevState.attempt + 1,
       feedbackMessage,
       block: absDiff === 0,
     }));
-  }
+  };
 
   render() {
-
-    const { allGuesses, attempt, feedbackMessage,show, block } = this.state
+    const { allGuesses, attempt, feedbackMessage, show, block } = this.state;
     const guessList = allGuesses.map((item, index) => (
-        <li key={index}>
-          <span>{item.guess}</span>
-        </li>
-      )
-    );
+      <li key={index}>
+        <span>{item.guess}</span>
+      </li>
+    ));
 
     return (
-      <Grid container style={{height: '100vh'}} justify="center" alignItems="center">
-        <Grid item xs={10} sm={3}>
-          <Paper style={{padding: '50px'}} elevation={6}>
-            <Typography align="center" variant="h3" gutterBottom > HOT or COLD </Typography>
-            <Divider style={{ margin: '20px 0'}} />
-            <div className={`feedback ${feedbackMessage[0].toLowerCase()}`}>
-              <h2 className='feedback-text'>{feedbackMessage}</h2>
-            </div>
-            <Form block={block} returnGuessToApp={value => this.updateAppState(value)}/>
-            <Progress feedbackMessage={feedbackMessage} attempt={attempt} guessList={guessList} />
-            <Button style={{ marginBottom: '15px' }} fullWidth variant="contained" color="primary" onClick={this.resetGame} >Reset Game</Button>
-            <Info show={show} onClose={this.handleClose}/>
-          </Paper>
-        </Grid>
-      </Grid>
-            )
+      <Container
+        maxWidth="xs"
+        style={{ display: "flex", alignItems: "center", height: "100vh" }}
+      >
+        <Paper style={{ width: "100%", padding: "50px 30px" }}>
+          <Typography align="center" variant="h3" gutterBottom>
+            {" "}
+            HOT or COLD{" "}
+          </Typography>
+          <Divider style={{ margin: "20px 0" }} />
+          <div className={`feedback ${feedbackMessage[0].toLowerCase()}`}>
+            <h2 className="feedback-text">{feedbackMessage}</h2>
+          </div>
+          <Form
+            block={block}
+            returnGuessToApp={(value) => this.updateAppState(value)}
+          />
+          <Progress
+            feedbackMessage={feedbackMessage}
+            attempt={attempt}
+            guessList={guessList}
+          />
+          <Button
+            style={{ marginBottom: "15px" }}
+            fullWidth
+            variant="contained"
+            color="primary"
+            onClick={this.resetGame}
+          >
+            Reset Game
+          </Button>
+          <Info show={show} onClose={this.handleClose} />
+        </Paper>
+      </Container>
+    );
   }
 }
 
